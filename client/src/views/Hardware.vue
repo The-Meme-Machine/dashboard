d01f913c65a2d19d60024cdfc74b0b4db47162cc
Iliyan <djrfung@outlook.com>
2020-06-23 01:54:09 -0700

    Add Vue project

---
+++ client/src/views/Hardware.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hardware">
+    <h1>Hardware Info</h1>
+    <ul>
+      <li></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  methods: {}
+};
+</script>
+
+<style>
+</style>
---
+++ routes/hardware.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const os = require('os');
+const utils = require('os-utils');
+const si = require('systeminformation');
+
+const memory = os.totalmem();
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send({
+    arch: os.arch(),
+    cpu: os.cpus(),
+    network: os.networkInterfaces(),
+    platform: os.platform(),
+    sysUptime: utils.sysUptime(),
+    processUptime: utils.processUptime(),
+    memory: memory / 1000000000
+  })
+})
+
+router.get('/detail', (req, res) => {
+  si.get({
+    cpu: 'manufacturer, brand, speed, speedmax, cores, physicalCores, cache',
+    system: 'manufacturer, model',
+    osInfo: 'platform, arch, distro, release',
+    mem: 'total',
+    memLayout: '*',
+    graphics: '*',
+    battery: '*',
+    diskLayout: '*'
+  }, (data) => {
+    res.send(data)
+  })
+})
+
+router.get('/static', (req, res) => {
+  si.getStaticData((data) => {
+    res.send(data)
+  })
+})
+
+router.get('/dynamic', (req, res) => {
+  si.getDynamicData((data) => {
+    res.send(data)
+  })
+})
+
+module.exports = router;
